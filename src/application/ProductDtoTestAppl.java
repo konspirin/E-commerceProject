a61b43686df14869cf75ea12bb76733db1d82420
Konstantin <konspirin@gmail.com>
2022-09-15 18:22:27 +0300

    make it better

--- src/application/ProductDtoTestAppl.java
+++ src/application/ProductDtoTestAppl.java
@@ -11,17 +11,17 @@ public class ProductDtoTestAppl {
 //		ProductDto prod = ProductDto.randomProductDto();
 //		System.out.println(prod);
 //		ProductDto.ProductDtoToJson(prod);
-//		ArticleDto art = ArticleDto.randomArticleDto();
-//		System.out.println(art);
-//		String res = ArticleDto.ArticleDtoToJson(art);
-//		System.out.println(res);
+		ArticleDto art = ArticleDto.randomArticleDto();
+		System.out.println(art);
+		String res = ArticleDto.ArticleDtoToJson(art);
+		System.out.println(res);
 		
-		OrderDtoRequest order = OrderDtoRequest.randomOrderDtoRequestCreater();
-		System.out.println(order);
-		String json = OrderDtoRequest.orderDtoRequestToJson(order);
-		System.out.println(json);
-		OrderDtoRequest order_new = OrderDtoRequest.JsonToOrderDtoRequest(json);
-		System.out.println(order_new);
+//		OrderDtoRequest order = OrderDtoRequest.randomOrderDtoRequestCreater();
+//		System.out.println(order);
+//		String json = OrderDtoRequest.orderDtoRequestToJson(order);
+//		System.out.println(json);
+//		OrderDtoRequest order_new = OrderDtoRequest.JsonToOrderDtoRequest(json);
+//		System.out.println(order_new);
 	}
 
 }
